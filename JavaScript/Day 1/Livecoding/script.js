4d9bdb94d953f23ed33d5344cc5fce5934031c4f
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-05-28 10:09:01 +0200

    v.05

--- JavaScript/Day 1/Livecoding/script.js
+++ JavaScript/Day 1/Livecoding/script.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+// DATA TYPES
+
+// string "" ''
+let first_name = "Andjelo";
+let last_name = "Saltin";
+console.log(first_name + last_name);
+
+
+// number
+let number = 2;
+console.log(number);
+
+// boolean true or false
+console.log(5 > 3);
+let gym = false;
+console.log(gym);
+
+// undefined (null), used rarly
+let age;
+console.log(age);
+
+//condition == <= >=
+
+let sum = 0;
+sum = 1 + 4 + 3; //8
+console.log(sum);
+
+
+let firstName = "Andjelo";
+let lastName = "Saltin";
+
+// In the console i want to see, Hello, my name is firstName lastName
+
+console.log("Hello, my name is " + firstName + " " + lastName);
+
+//bettwe way
+console.log(`Hello, my name is ${firstName} ${lastName}`);
