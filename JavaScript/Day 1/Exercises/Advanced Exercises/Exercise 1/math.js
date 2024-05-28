b2eb9fd2a5c8de41a49dd924f3a292334257a0ee
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-05-28 14:32:09 +0200

    Exercice 2 done

---
+++ JavaScript/Day 1/Exercises/Advanced Exercises/Exercise 1/math.js
@@ -0,0 +1,19 @@
+let a = 7;
+let b = 14;
+let c = "21";
+let d = '36';
+let e = 42;
+
+let f = '1';
+let g = 15;
+let h = 8;
+let i = "1";
+
+
+let var1 = a + b + Number(c) + Number(d) + e;
+
+let var2 = Number(f) * g * h * Number(i);
+
+let var3 = var1 / var2;
+
+document.write(`Math result is ${var3}`);
