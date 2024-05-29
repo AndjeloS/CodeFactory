f8a761a2aec9d0c0ed97513cb179e21be7d36a89
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-05-29 12:36:16 +0200

    Exercise done

--- JavaScript/Day 2/Exercises/Basic Exercises/Exercise 1/temp_generator.js
+++ JavaScript/Day 2/Exercises/Basic Exercises/Exercise 1/temp_generator.js
@@ -1,2 +1,8 @@
 let random = Math.floor((Math.random() * 31) - 5);
 console.log(random);
+
+if (random >= -5 && random <= 10) {
+    console.log("The Weather is cold.");
+} else {
+    console.log("The weather is moderate.");
+}
