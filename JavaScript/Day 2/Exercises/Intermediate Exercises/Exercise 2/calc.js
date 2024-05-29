13b7031fb8f292e2e3199bfc04c446868a3fcfe3
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-05-29 15:36:49 +0200

    Exercise 2 done

--- JavaScript/Day 2/Exercises/Intermediate Exercises/Exercise 2/calc.js
+++ JavaScript/Day 2/Exercises/Intermediate Exercises/Exercise 2/calc.js
@@ -1,14 +1,10 @@
-function avgGrade(math, physics, english) {
+function Grade(math, physics, english) {
     let sum = math + physics + english;
     let avg = sum / 3;
-    if (sum != Number || avg != Number) {
-        console.log("Please enter only numbers!");
-    } else {
-        return `Sum: ${sum} \n Average: ${avg}`
-    }
 
+    return `Sum: ${sum} \n Average: ${avg}`
 }
 
 console.log(
-    avgGrade(3, 4, 5)
+    Grade(3, 4, 5)
 );
