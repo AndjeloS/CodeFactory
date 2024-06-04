bca0ec72362f199992aa7df17b0985f518aeb614
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-04 15:43:44 +0200

    deletet not working code

--- JavaScript/Day 4/Exercises/Advanced Exercises/Exercise 1/script.js
+++ JavaScript/Day 4/Exercises/Advanced Exercises/Exercise 1/script.js
@@ -22,30 +22,6 @@ function BgGray() {
 let DELAY = 700, clicks = 0, timer = null;
 
 function BgBlue() {
-    ("a").on("click", function (e) {
-
-        clicks++;  //count clicks
-
-        if (clicks === 1) {
-
-            timer = setTimeout(function () {
-
-                alert("Single Click");  //perform single-click action    
-                clicks = 0;             //after action performed, reset counter
-
-            }, DELAY);
-
-        } else {
-
-            clearTimeout(timer);    //prevent single-click action
-            alert("Double Click");  //perform double-click action
-            clicks = 0;             //after action performed, reset counter
-        }
-
-    })
-        .on("dblclick", function (e) {
-            e.preventDefault();  //cancel system double-click event
-        });
     let color2 = "blue";
     document.getElementById("circle").style.background = color2;
 };
