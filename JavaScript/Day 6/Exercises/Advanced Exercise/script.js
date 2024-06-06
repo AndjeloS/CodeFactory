55f2211ec0593bd64a757a16697a32c17a91a5ce
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-06 14:35:16 +0200

    Exercise done

---
+++ JavaScript/Day 6/Exercises/Advanced Exercise/script.js
@@ -0,0 +1,43 @@
+
+fetch('./list.json')
+    .then(function (response) {
+        return response.json()
+    }).then(function (result) {
+        for (let val of result.booklist.data
+        ) {
+            document.getElementById("result").innerHTML += `<div>
+            <div class="card" style="width: 18rem;">
+            <img src="${val.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${val.title}</h5>
+              <p class="card-text">${val.author}</p>
+              <p class="card-text">Read book: ${val.read}</p>
+              <a href="#details" class="btn btn-primary mybtn">Show details</a>
+            </div>
+          </div></div>
+            `;
+
+            let btns = document.querySelectorAll(".mybtn");
+            btns.forEach((element, i) => {
+                element.addEventListener("click", function () {
+                    document.getElementById(
+                        "details"
+                    ).innerHTML = `
+                    <div id="decr-body" class="card">
+                        <img src="${val.img}" class="card-img-top" id="desc-img">
+                        <div class="card-body">
+                        <h5 class="card-title">${val.title}</h5>
+                        <p class="card-text">${val.author}</p>
+                        <a href="${val.buy}" class="btn btn-primary btnRemoveDetails">Go and buy it!</a>
+                        </a>
+                    </button>
+                        </div>
+                    </div>;`
+                })
+            })
+        };
+    });
+
+
+
+
