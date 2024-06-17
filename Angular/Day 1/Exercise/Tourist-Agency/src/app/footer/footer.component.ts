9413fe416184d87a5071033326b705bae92156bf
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-17 15:00:14 +0200

    used interpolation for footer

--- Angular/Day 1/Exercise/Tourist-Agency/src/app/footer/footer.component.ts
+++ Angular/Day 1/Exercise/Tourist-Agency/src/app/footer/footer.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  copyright: string = 'Copyright by Travel Agency - 2024';
 }
