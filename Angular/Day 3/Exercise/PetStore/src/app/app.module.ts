5d78be1de5e425d29fed64dc0af311135502ee96
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-19 13:16:43 +0200

    added home, details and cart

--- Angular/Day 3/Exercise/PetStore/src/app/app.module.ts
+++ Angular/Day 3/Exercise/PetStore/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    DetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
