b89d3f18b1d307eeb7fcdad2927075fdfaf5bd53
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-19 14:57:25 +0200

    added pet details

--- Angular/Day 3/Exercise/PetStore/src/app/details/details.component.ts
+++ Angular/Day 3/Exercise/PetStore/src/app/details/details.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { pets } from '../pets';
+import { IPet } from '../pet.model';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
+  pets: IPet[];
+  pet: IPet;
 
+  constructor(private route: ActivatedRoute, private cartService: CartService) {
+    this.pets = pets;
+    const id = +this.route.snapshot.params['index'];
+    this.pet = this.pets[id];
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.pet);
+  }
 }
