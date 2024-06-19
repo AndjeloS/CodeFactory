937092fe6b4690615f6a18f3f83db303b64cc1bf
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-19 13:29:39 +0200

    added routing

--- Angular/Day 3/Exercise/PetStore/src/app/app-routing.module.ts
+++ Angular/Day 3/Exercise/PetStore/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'details/:index',
+    component: DetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
