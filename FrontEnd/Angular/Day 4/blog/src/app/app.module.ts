b9bb4822a4b0a482445e13cc3c06d89038326b63
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-20 12:43:27 +0200

    created all components

--- FrontEnd/Angular/Day 4/blog/src/app/app.module.ts
+++ FrontEnd/Angular/Day 4/blog/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
