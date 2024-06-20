66c82fabfad565e6327d4ac3c25c9d76b25369ee
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-20 13:45:31 +0200

    porifle image link was not correct

--- FrontEnd/Angular/Day 4/blog/src/app/blog.ts
+++ FrontEnd/Angular/Day 4/blog/src/app/blog.ts
@@ -12,7 +12,7 @@ export const blogs: IBlog[] = [
   {
     topic: 'Price of Food',
     username: 'Jimmy Bill Bob',
-    profileImage: '/profilepic3.jpg',
+    profileImage: '/profile_images/profilepic3.jpg',
     postDate: '',
     postTime: '',
     blog: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis aperiam dolorum reprehenderit modi. Accusamus suscipit mollitia expedita fugiat dolor distinctio, repudiandae laboriosam harum aspernatur veritatis, optio tenetur repellendus dolore!Cupiditate ab, beatae et placeat a dignissimos asperiores, eligendi iusto, error rerum sint sapiente! Natus tempore consectetur deserunt id consequuntur doloremque quisquam inventore unde, quam sint, possimus doloribus beatae culpa!Qui, odio! Blanditiis sit nam totam ullam iste iure, eum dolores qui voluptas saepe praesentium aut autem atque odio delectus soluta accusamus unde veniam est facilis nulla impedit labore corrupti.Nihil a unde quidem beatae eveniet quasi, optio nemo doloremque voluptatibus, iusto maxime esse quis recusandae possimus obcaecati aliquid. Officia omnis totam veritatis a magnam consequuntur ad dolorem quae minus!Et deleniti fugiat assumenda facilis laborum error? Possimus excepturi quia molestias deleniti dolore nesciunt qui aliquam optio nisi, soluta officia pariatur facere architecto commodi expedita placeat cum quibusdam eaque. Amet.Provident placeat vitae temporibus, nihil laudantium non a minus cumque veritatis animi saepe. A modi quasi, nam, sequi provident aliquam libero accusamus magni veniam autem temporibus, fugiat corrupti culpa expedita.',
@@ -21,7 +21,7 @@ export const blogs: IBlog[] = [
   {
     topic: 'Dogs vs Cats! The better pet!',
     username: 'Frank Sinotra',
-    profileImage: '/profilepic1.jpg',
+    profileImage: '/profile_images/profilepic1.jpg',
     postDate: '',
     postTime: '',
     blog: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis aperiam dolorum reprehenderit modi. Accusamus suscipit mollitia expedita fugiat dolor distinctio, repudiandae laboriosam harum aspernatur veritatis, optio tenetur repellendus dolore!Cupiditate ab, beatae et placeat a dignissimos asperiores, eligendi iusto, error rerum sint sapiente! Natus tempore consectetur deserunt id consequuntur doloremque quisquam inventore unde, quam sint, possimus doloribus beatae culpa!Qui, odio! Blanditiis sit nam totam ullam iste iure, eum dolores qui voluptas saepe praesentium aut autem atque odio delectus soluta accusamus unde veniam est facilis nulla impedit labore corrupti.Nihil a unde quidem beatae eveniet quasi, optio nemo doloremque voluptatibus, iusto maxime esse quis recusandae possimus obcaecati aliquid. Officia omnis totam veritatis a magnam consequuntur ad dolorem quae minus!Et deleniti fugiat assumenda facilis laborum error? Possimus excepturi quia molestias deleniti dolore nesciunt qui aliquam optio nisi, soluta officia pariatur facere architecto commodi expedita placeat cum quibusdam eaque. Amet.Provident placeat vitae temporibus, nihil laudantium non a minus cumque veritatis animi saepe. A modi quasi, nam, sequi provident aliquam libero accusamus magni veniam autem temporibus, fugiat corrupti culpa expedita.',
@@ -30,7 +30,7 @@ export const blogs: IBlog[] = [
   {
     topic: 'Current World Affairs',
     username: 'Snake Tongue',
-    profileImage: '/profilepic2.jpg',
+    profileImage: '/profile_images/profilepic2.jpg',
     postDate: '',
     postTime: '',
     blog: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis aperiam dolorum reprehenderit modi. Accusamus suscipit mollitia expedita fugiat dolor distinctio, repudiandae laboriosam harum aspernatur veritatis, optio tenetur repellendus dolore!Cupiditate ab, beatae et placeat a dignissimos asperiores, eligendi iusto, error rerum sint sapiente! Natus tempore consectetur deserunt id consequuntur doloremque quisquam inventore unde, quam sint, possimus doloribus beatae culpa!Qui, odio! Blanditiis sit nam totam ullam iste iure, eum dolores qui voluptas saepe praesentium aut autem atque odio delectus soluta accusamus unde veniam est facilis nulla impedit labore corrupti.Nihil a unde quidem beatae eveniet quasi, optio nemo doloremque voluptatibus, iusto maxime esse quis recusandae possimus obcaecati aliquid. Officia omnis totam veritatis a magnam consequuntur ad dolorem quae minus!Et deleniti fugiat assumenda facilis laborum error? Possimus excepturi quia molestias deleniti dolore nesciunt qui aliquam optio nisi, soluta officia pariatur facere architecto commodi expedita placeat cum quibusdam eaque. Amet.Provident placeat vitae temporibus, nihil laudantium non a minus cumque veritatis animi saepe. A modi quasi, nam, sequi provident aliquam libero accusamus magni veniam autem temporibus, fugiat corrupti culpa expedita.',
