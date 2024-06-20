d97b88393d05eece4d0f22cf6ac26c67c3d6432b
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-06-20 12:57:35 +0200

    connected component to index

--- FrontEnd/Angular/Day 4/blog/src/app/app-routing.module.ts
+++ FrontEnd/Angular/Day 4/blog/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'blog',
+    component: BlogPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'blog',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
