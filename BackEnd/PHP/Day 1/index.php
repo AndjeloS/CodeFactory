da9254f4a74be2bdbd6f262199fb224be48aee3d
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-07-16 14:50:03 +0200

    added php Day 1 exercises and solutions

---
+++ BackEnd/PHP/Day 1/index.php
@@ -0,0 +1,54 @@
+<?php 
+echo "<link rel='stylesheet' type='text/css' href='style.css' />";
+
+echo "Basic Exercises:";
+
+echo "<br>";
+echo "<br>";
+
+
+
+#Exercise 1
+echo"Exercise 1"; 
+
+
+
+$fullname = "Andjelo SALTIN";
+
+echo "<h1>Hi, my name is $fullname!</h1>";
+
+echo "<br>";
+echo "<br>";
+
+
+
+#Exercise 2
+echo"Exercise 2"; 
+
+echo "<br>";
+
+$name = "John";
+$job = "Programmer";
+$age = 30;
+
+echo "Hi, my name is $name, i am $age years old and work as a $job!";
+
+echo "<br>";
+echo "<br>";
+
+
+
+#Exercise 3
+echo"Exercise 3"; 
+
+$player = ["Mark", "John", "Georg", "Lisa"];
+echo "<br>";
+
+echo "The third player in the team is $player[2].";
+
+#Advanced Exercise
+echo "<br>";
+echo "<br>";
+echo"Advanced Exercise:";
+echo "<br>";
+echo "<br>";
