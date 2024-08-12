260140e92272b45e10ce5817813259956ee188bf
AndjeloS <sreten.andjelo.saltin@gmail.com>
2024-08-12 15:29:52 +0200

    added livecoding day 6

---
+++ BackEnd/Symfony/Day 6/LiveCoding_day6/src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            MoneyField::new('price')->setCurrency('EUR')->setStoredAsCents(false),
+            TextField::new('image')->hideOnIndex(),
+            AssociationField::new('category')->autocomplete(),
+        ];
+    }
+}
